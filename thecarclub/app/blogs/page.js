a547c8f8bb124d7beb5d875a3ba3bd5941d6d837
rajamuhammadali628-hu <rajamuhammadali628@gmail.com>
2025-08-27 23:16:37 +0500

    Adding Images and Blog page's content

--- thecarclub/app/blogs/page.js
+++ thecarclub/app/blogs/page.js
@@ -5,7 +5,41 @@ import Image from 'next/image';
 const  Blogs= () => {
   return (
     <>
-    
+      <br />
+      <br />
+      <div className={styles.blogcontent}>
+        <div className={styles.blogsubcontent}>
+          <h1>Welcome to the Blogs Area</h1>
+          <h2>Here you can see information about the blogs.</h2>
+        </div>
+      </div>
+      <br />
+      <h3 className={styles.heading}>Sports Cars</h3>
+      <br />
+      <br />
+      <div className={styles.blogmaincontent}>
+        <div className={styles.blogImage1}></div>
+        <div className={styles.blogImage2}></div>
+    </div>
+      <br />
+      <div className={styles.blogmaincontent}>
+        <div className={styles.blogImage3}></div>
+        <div className={styles.blogImage4}></div>
+      </div>
+      <br />
+      <h3 className={styles.heading}>Luxary Cars</h3>
+      <br />
+      <br />
+    <div className={styles.blogmaincontent}>
+        <div className={styles.blogImage5}></div>
+        <div className={styles.blogImage6}></div>
+    </div>
+      <br />
+      <div className={styles.blogmaincontent}>
+        <div className={styles.blogImage7}></div>
+        <div className={styles.blogImage8}></div>
+      </div>
+      <br />
     </>
   )
 }
