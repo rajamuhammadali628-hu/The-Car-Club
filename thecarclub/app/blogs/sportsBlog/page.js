f4a4102a3f53513385b756034e47d8d5c31cc105
rajamuhammadali628-hu <rajamuhammadali628@gmail.com>
2025-08-29 10:34:28 +0500

    Adding more information

--- thecarclub/app/blogs/sportsBlog/page.js
+++ thecarclub/app/blogs/sportsBlog/page.js
@@ -13,9 +13,9 @@ const SportsBlog = () => {
             <h2>Here you can see the Information about the sport cars</h2>
         </div>
       </div>
-      <div className={styles.infoSection}>
-        <div className={styles.BMWsection}>
-          <div className={styles.BMWsubImage}>
+      <br />
+      <br />
+        <div className={styles.infosection}>
               <Image
             src="/BMW-sports.jpg"
             alt="Eyes of BMW"
@@ -23,9 +23,7 @@ const SportsBlog = () => {
             height={150}
             className={styles.heroImage}
           />
-          </div>
-          <div className={styles.BMWsubpara}>
-            <p>The BMW M4 Sports represents the peak of automotive excellence, masterfully combining 
+            <p><span className={styles.headings}>BMW M4 Sports</span><br />The BMW M4 Sports represents the peak of automotive excellence, masterfully combining 
             high-performance engineering with elegant design. Powered by a 3.0-liter twin-turbocharged 
             inline-six engine generating 503 horsepower, it accelerates from 0 to 60 mph in just 3.8 seconds. 
             The vehicle features an aggressive exterior with its signature kidney grille and carbon-fiber roof, 
@@ -33,9 +31,28 @@ const SportsBlog = () => {
             driving experience. Inside, the M4 blends race-inspired sport seats with luxury amenities and advanced
              technology. This German-engineered masterpiece perfectly balances track-ready performance with 
              everyday practicality, making each drive a memorable experience.</p>
-          </div>
         </div>
-      </div>
+        <br />
+      <br />
+        <div className={styles.infosection}>
+            <p><span className={styles.headings}>Toyota Supra MK4</span><br />The Toyota Supra MK4, officially the 
+            fourth-generation (A80) Supra produced from 1993 to 2002, is an automotive icon revered for its 
+            engineering brilliance and legendary performance. Its heart and claim to fame was the formidable
+             2JZ-GTE engine, a 3.0-liter twin-turbocharged inline-six masterpiece celebrated for its immense 
+             strength and staggering potential for power with minimal modifications. Cloaked in a timeless, 
+             aerodynamic body and featuring a technologically advanced interior for its era, the Supra was a 
+             true grand tourer. However, its near-mythical status was cemented by its dominant presence in 
+             motorsports and its starring role in the The Fast and the Furious film franchise, transforming it 
+             from a high-performance Japanese sports car into a global phenomenon and a highly sought-after 
+             collector's item that continues to captivate enthusiasts worldwide.</p>
+                 <Image
+            src="/Supra1.jpg"
+            alt="Eyes of BMW"
+            width={300}
+            height={150}
+            className={styles.heroImage}
+          />
+        </div>
     </>
   )
 }
